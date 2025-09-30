cef87f01508800af6cd785d1937383d92f39d8a4
Santiagoaghhh <santaguilar17@gmail.com>
2025-09-29 19:47:32 -0500

    feat: :clown_face: Swaggger

--- swagger.js
+++ swagger.js
@@ -12,7 +12,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:4000/api/v1",
+        url: "https://proyecto-express-backend-aparicio-s.vercel.app/",
       },
     ],
   },
